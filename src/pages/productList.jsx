b081110f2d53b23f254f6d074706031d0df71629
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2023-01-11 20:27:47 -0500

    saving

---
+++ src/pages/productList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const productList = () => {
+    return (
+        <div>
+            <h1>Product list page</h1>
+        </div>
+    );
+};
+
+export default productList;
